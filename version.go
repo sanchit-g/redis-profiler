7307f7b4f723a9dbc3eb8b566bfd41b7cafc555c
Sanchit Gupta <56797057+sanchit-g@users.noreply.github.com>
2026-03-02 01:19:47 +0530

    chore(main): release 0.1.2-beta

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package main
 
 // Version is the current version of redis-profiler
 // Automatically updated by release-please on each release
-const Version = "0.1.1-beta" // x-release-please-version
+const Version = "0.1.2-beta" // x-release-please-version
