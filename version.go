99544d7117223d3a8436fff27da77350d5101daf
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2026-03-02 01:01:14 +0530

    fix: verify release-please updates version.go correctly

--- version.go
+++ version.go
@@ -1,5 +1,5 @@
 package main
 
-// Version is the current version of redis-profiler.
-// Automatically updated by release-please on each release.
+// Version is the current version of redis-profiler
+// Automatically updated by release-please on each release
 const Version = "0.1.1-beta"
